a8f861d533397db0d63ee851ccd030043f91cff9
Miguel Angel Alanis Montes <miguel_aam@hotmail.com>
2017-11-03 10:19:11 -0600

    Definición de un wrapper de transacción

---
+++ Lab2/TransactionWrapper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
+namespace AutoCADAPI.Lab2
+{
+    public class TransactionWrapper
+    {
+        public Document Doc
+        {
+            get { return Application.DocumentManager.MdiActiveDocument; }
+        }
+
+        public Editor Ed
+        {
+            get { return Doc.Editor; }
+        }
+        //La definición de la función tipo Transaction Handler
+        public delegate Object TransactionHandler(Document doc, Transaction tr, Object[] input);
+        //La variable tipo función y tipo Transaction Handler
+        public TransactionHandler TransactionTask;
+
+        public Object Run(TransactionHandler task, Object[] input)
+        {
+            Database dwg = this.Doc.Database;
+            Object result;
+            using (Transaction tr = dwg.TransactionManager.StartTransaction())
+            {
+                try
+                {
+                    result = task(this.Doc, tr, input);
+                    tr.Commit();
+                }
+                catch (System.Exception exc)
+                {
+                    this.Ed.WriteMessage(exc.Message);
+                    tr.Abort();
+                    throw exc;
+                }
+            }
+            return result;
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
